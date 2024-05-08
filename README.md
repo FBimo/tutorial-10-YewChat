124f47c927aa781c8fa7b7c9bb1a24e6e3793bf0
FBimo <f.bimo31@gmail.com>
2024-05-08 14:26:17 +0700

    Experiment 3.2: Be Creative!

--- README.md
+++ README.md
@@ -7,3 +7,12 @@
 ![3.1. Original code - 1](image-1.png)
 
 </details>
+
+<details>
+<summary> 3.2. Add some creativities to the webclient </summary>
+
+![3.2. Add some creativities to the webclient](image-2.png)
+
+Terlihat pada gambar di atas, adanya perubahan pada _default_ dari _profile picture_ menjadi gambar yang baru. 
+
+</details>
--- src/components/chat.rs
+++ src/components/chat.rs
@@ -93,8 +93,7 @@ impl Component for Chat {
                             .map(|u| UserProfile {
                                 name: u.into(),
                                 avatar: format!(
-                                    "https://avatars.dicebear.com/api/adventurer-neutral/{}.svg",
-                                    u
+                                    "https://i.postimg.cc/RVGz52gb/janaeither-t-profile-gif.gif"
                                 )
                                 .into(),
                             })
